ba65221aee3079477e845aac337012f523e9dae7
Nael-Ballo <ton_email@github.com>
2025-09-15 15:07:53 +0200

    Correction

--- main.py
+++ main.py
@@ -2,32 +2,21 @@ from math import sqrt
 
 #### Fonction secondaire
 
-
 def isprime(p):
-    for b in range(2,p):
-        if p%b == 0:
+    if p < 2:
+        return False
+    for b in range(2, int(sqrt(p)) + 1):
+        if p % b == 0:
             return False
     return True
-pass
-
 
 #### Fonction principale
 
-
 def main():
-
-    # vos appels à la fonction secondaire ici
-
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
 
-
 if __name__ == "__main__":
-<<<<<<< HEAD
     main()
-=======
-    main()
->>>>>>> 3f224bb (Premier commit)
